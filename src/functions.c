5173f33e88c832ac2ab4977ea162e2057368ad15
dcorn-stone <gayi_chen@outlook.com>
2025-06-20 23:24:08 +0800

    add file type detection

--- src/functions.c
+++ src/functions.c
@@ -1,24 +1,24 @@
 #include "functions.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-int load_config(){
+int load_config() {
   FILE *conf = fopen("../server.conf", "r");
-  if (!conf){
+  if (!conf) {
     return -1;
   }
 
   char line[512];
-  while(fgets(line, sizeof(line), conf)){
+  while (fgets(line, sizeof(line), conf)) {
     char key[128], value[384];
-    if (sscanf(line, "%127[^=]=%383[^\n]", key, value) == 2){
-      if (strcmp(key, "port") == 0){
+    if (sscanf(line, "%127[^=]=%383[^\n]", key, value) == 2) {
+      if (strcmp(key, "port") == 0) {
         config.port = atoi(value);
-      } else if (strcmp(key, "listening_address") == 0){
+      } else if (strcmp(key, "listening_address") == 0) {
         strncpy(config.address, value, sizeof(config.address));
         config.address[sizeof(config.address) - 1] = '\0';
-      } else if (strcmp(key, "document_root") == 0){
+      } else if (strcmp(key, "document_root") == 0) {
         strncpy(config.document_root, value, sizeof(config.document_root));
       }
     }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 // importing libraries
 #include "functions.h"
+#include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stddef.h>
@@ -7,9 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-
 
 // buffer chunk size
 #define BUFFER_SIZE 4096
@@ -22,11 +21,9 @@ server_config config;
 
 // main function
 int main() {
-    
 
-  //read configuration
+  // read configuration
   load_config();
-
 
   // creating connection
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -117,8 +114,6 @@ int main() {
   return 0;
 }
 
-
-
 // serve supported file types to client
 int serve_file(char *buf_ptr, int client_fd) {
 
@@ -129,9 +124,32 @@ int serve_file(char *buf_ptr, int client_fd) {
   // read the method and path into the variables
   sscanf(buf_ptr, "%s %s", method, path);
 
-
   // if using GET to the root domain
   if (strcmp(method, "GET") == 0 && strcmp(path, "/") == 0) {
+
+    // detect file type
+    char file_type[128];
+    const char *ext = strrchr(path, '.');
+    if (!ext) {
+      strcpy(file_type, "application/octet-stream"); // fall-back MIME type
+    }
+
+    if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0)
+      strcpy(file_type, "text/html");
+    else if (strcmp(ext, ".css") == 0)
+      strcpy(file_type, "text/css");
+    else if (strcmp(ext, ".js") == 0)
+      strcpy(file_type, "application/javascript");
+    else if (strcmp(ext, ".json") == 0)
+      strcpy(file_type, "application/json");
+    else if (strcmp(ext, ".png") == 0)
+      strcpy(file_type, "image/png");
+    else if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0)
+      strcpy(file_type, "image/jpeg");
+    else if (strcmp(ext, ".gif") == 0)
+      strcpy(file_type, "image/gif");
+    else
+      strcpy(file_type, "application/octet-stream"); // default MIME type
 
     // serve the index.html file
     FILE *fptr = fopen(config.document_root, "rb");
@@ -145,10 +163,11 @@ int serve_file(char *buf_ptr, int client_fd) {
       // send header
       char header[256];
       snprintf(header, sizeof(header),
-          "HTTP/1.1 200 OK\r\n"
-          "Content-Type: text/html\r\n"
-          "Content-Length: %ld\r\n"
-          "\r\n", filesize);
+               "HTTP/1.1 200 OK\r\n"
+               "Content-Type: %s\r\n"
+               "Content-Length: %ld\r\n"
+               "\r\n",
+               file_type, filesize);
 
       write(client_fd, header, strlen(header));
 
@@ -164,7 +183,7 @@ int serve_file(char *buf_ptr, int client_fd) {
             // Handle write error (e.g., client disconnected)
             break;
           }
-        total_sent += sent;
+          total_sent += sent;
         }
       }
 
@@ -177,7 +196,6 @@ int serve_file(char *buf_ptr, int client_fd) {
       write(client_fd, not_found, strlen(not_found));
       printf("File not found\n");
     }
-
   } else {
     // not GET /, send 404
     const char *not_found = "HTTP/1.1 404 Not Found\r\nContent-Type: "
